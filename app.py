a373fef2e9c861ce54a067a7b6b173353c778808
GamerNoTitle <GamerNoTitle@outlook.com>
2023-06-18 18:43:06 +0800

    Added wheels

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 import re
+import requests
 from flask import Flask, render_template, send_from_directory
 from utils.mcclient import QueryClient
 
@@ -13,6 +14,7 @@ with open('config.yml', encoding='utf8') as f:
 mc_show_info = conf['server']['show-info']
 mc_host = conf['server']['host']
 mc_port = conf['server']['port']
+mc_query = conf['server']['query']
 mc_name = conf['server']['name']
 mc_logo = conf['server']['logo']
 mc_preview_title = conf['server']['preview']['title']
@@ -23,9 +25,7 @@ join_content = conf['server']['contact']['content']
 host = conf['web']['host']
 port = conf['web']['port']
 # for Minecraft Java servers (needs to be enabled on the server)
-query_client = QueryClient(mc_host, port=mc_port)
-res = query_client.get_status()
-print(res.res, res.game_id, res.gametype, res.host, res.hostip, res.hostport, res.map, res.motd, res.players.list, res.plugins, res.port, res.raw_res, res.timestamp, res.version.name, sep='\n')
+query_client = QueryClient(mc_host, port=mc_query)
 
 @ app.route('/assets/<path:filename>')
 def serve_static(filename):
@@ -37,8 +37,21 @@ def serve_images(filename):
 
 @ app.route('/')
 def home():
+    res = query_client.get_status()
     cleaned_motd = re.sub(r'§[a-f0-9klmnor]', '', res.motd)
     cleaned_motd = re.sub(r'[^a-zA-Z0-9\s]', '', cleaned_motd)
+    player_list = []
+    for player in res.players.list:
+        try:
+            data = requests.get(f'https://playerdb.co/api/player/minecraft/{player}', timeout=5).json()
+            if data['success']:
+                uuid = data['data']['player']['id']
+                img = f'https://crafatar.com/renders/head/{uuid}'
+            else:
+                img = 'https://crafatar.com/renders/head/aaaaaaaa-cf6b-4485-bef9-3957e7b7f509'
+        except requests.exceptions.ReadTimeout:
+            img = 'https://crafatar.com/renders/head/aaaaaaaa-cf6b-4485-bef9-3957e7b7f509'
+        player_list.append({'name': player, 'img': img})
     return render_template('index.html',
                            name = mc_name,
                            host = mc_host,
@@ -51,7 +64,8 @@ def home():
                            preview_title = mc_preview_title,
                            preview_descr = mc_preview_descr,
                            preview_images = mc_preview_images,
-                           join_content = join_content)
+                           join_content = join_content,
+                           player_list = player_list)
 
 if __name__ == '__main__':
     app.run(host, port, debug=1)
